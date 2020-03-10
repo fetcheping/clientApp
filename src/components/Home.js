4be9c34611409cd438460b8225efb14a5aa11412
Cabrel <cabreltchomte97@gmail.com>
2020-03-10 21:13:22 +0100

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  displayName = Home.name;
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <p>Welcome to your new single-page application, built with:</p>
+        <ul>
+          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
+          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+        </ul>
+      </div>
+    );
+  }
+}
